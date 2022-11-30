68a4c316fceda1dd890a3acba0f0ef0275e21099
Helen Lehrer <hlehrer@outlook.com>
2022-11-29 20:08:52 -0800

    Add actiontypes

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -1,5 +1,6 @@
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 
+
 describe("formVisibleReducer", () => {
 
   test('Should return default state if no action type is recognized', () => {
--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -5,6 +5,7 @@ import TicketDetail from './TicketDetail';
 import EditTicketForm from './EditTicketForm';
 import { connect } from 'react-redux';
 import PropTypes from "prop-types";
+import * as a from './../actions';
 
 class TicketControl extends React.Component {
   constructor(props) {
@@ -25,18 +26,9 @@ class TicketControl extends React.Component {
 
   handleAddingNewTicketToList = (newTicket) => {
     const { dispatch } = this.props;
-    const { id, names, location, issue } = newTicket;
-    const action = {
-      type: 'ADD_TICKET',
-      id: id,
-      names: names,
-      location: location,
-      issue: issue,
-    }
+    const action = a.addTicket(newTicket);
     dispatch(action);
-    const action2 = {
-      type: 'TOGGLE_FORM'
-    }
+    const action2 = a.toggleForm();
     dispatch(action2);
     // this.setState({formVisibleOnPage: false});
     // const newMainTicketList = this.state.mainTicketList.concat(newTicket);
@@ -52,9 +44,7 @@ class TicketControl extends React.Component {
       });
     } else {
       const { dispatch } = this.props;
-      const action = {
-        type: 'TOGGLE_FORM'
-      }
+      const action = a.toggleForm();
       dispatch(action);
     // this.setState(prevState =>({formVisibleOnPage: !prevState.formVisibleOnPage}));
     }
@@ -62,10 +52,7 @@ class TicketControl extends React.Component {
 
   handleDeletingTicket = (id) => {
     const { dispatch } = this.props;
-    const action = {
-      type: 'DELETE_TICKET',
-      id: id
-    }
+    const action = a.deleteTicket(id);
     dispatch(action);
     this.setState({selectedTicket: null});
     // const newMainTicketList = this.state.mainTicketList.filter(ticket => ticket.id !== id);
@@ -77,19 +64,12 @@ class TicketControl extends React.Component {
 
   handleEditingTicketInList = (ticketToEdit) => {
     const { dispatch } = this.props;
-    const { id, names, location, issue } = ticketToEdit;
-    const action = {
-      type: 'ADD_TICKET',
-      id: id,
-      names: names,
-      location: location,
-      issue: issue,
-    }
+    const action = a.addTicket(ticketToEdit)
     dispatch(action);
     this.setState({
       editing: false,
       selectedTicket: null
-    })
+    });
     // const editedMainTicketList = this.state.mainTicketList.filter(ticket => ticket.id !== this.state.selectedTicket.id).concat(ticketToEdit);
     // this.setState({
     //   mainTicketList: editedMainTicketList,
